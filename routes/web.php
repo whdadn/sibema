2b89de112dcd3344f3df64cc639068b88782997a
whdadn <wahidadinugrohoo@gmail.com>
2023-06-18 20:14:45 +0800

    memperbaiki route

--- routes/web.php
+++ routes/web.php
@@ -22,44 +22,46 @@ Route::get('/', function () {
 
 
 // Bagian Mahasiswa
-Route::get('/dashboard', function () {
-    return view('dashboard.menuMhs.dasboardMhs');
-});
-Route::get('/dashboard/uploadTa', function () {
-    return view('dashboard.menuMhs.uploadTa');
-});
-Route::get('/dashboard/tambahDokTa', function () {
-    return view('dashboard.menuMhs.tambahDokTa');
-});
-Route::get('/dashboard/perbaruiDokTa', function () {
-    return view('dashboard.menuMhs.perbaruiDokTa');
-});
-Route::get('/dashboard/uploadRegis', function () {
-    return view('dashboard.menuMhs.uploadRegis');
-});
-Route::get('/dashboard/tambahDokRegis', function () {
-    return view('dashboard.menuMhs.tambahDokRegis');
-});
-Route::get('/dashboard/perbaruiDokRegis', function () {
-    return view('dashboard.menuMhs.PerbaruiDokRegis');
-});
-Route::get('/dashboard/uploadPerpus', function () {
-    return view('dashboard.menuMhs.uploadPerpus');
-});
-Route::get('/dashboard/tambahDokPerpus', function () {
-    return view('dashboard.menuMhs.tambahDokPerpus');
-});
-Route::get('/dashboard/perbaruiDokPerpus', function () {
-    return view('dashboard.menuMhs.perbaruiDokPerpus');
-});
-Route::get('/dashboard/uploadAkademik', function () {
-    return view('dashboard.menuMhs.uploadAkademik');
-});
-Route::get('/dashboard/tambahDokAkademik', function () {
-    return view('dashboard.menuMhs.tambahDokAkademik');
-});
-Route::get('/dashboard/perbaruiDokAkademik', function () {
-    return view('dashboard.menuMhs.perbaruiDokAkademik');
+Route::prefix('/dashboard')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard.menuMhs.dasboardMhs');
+    });
+    Route::get('/uploadTa', function () {
+        return view('dashboard.menuMhs.uploadTa');
+    });
+    Route::get('/tambahDokTa', function () {
+        return view('dashboard.menuMhs.tambahDokTa');
+    });
+    Route::get('/perbaruiDokTa', function () {
+        return view('dashboard.menuMhs.perbaruiDokTa');
+    });
+    Route::get('/uploadRegis', function () {
+        return view('dashboard.menuMhs.uploadRegis');
+    });
+    Route::get('/tambahDokRegis', function () {
+        return view('dashboard.menuMhs.tambahDokRegis');
+    });
+    Route::get('/perbaruiDokRegis', function () {
+        return view('dashboard.menuMhs.PerbaruiDokRegis');
+    });
+    Route::get('/uploadPerpus', function () {
+        return view('dashboard.menuMhs.uploadPerpus');
+    });
+    Route::get('/tambahDokPerpus', function () {
+        return view('dashboard.menuMhs.tambahDokPerpus');
+    });
+    Route::get('/perbaruiDokPerpus', function () {
+        return view('dashboard.menuMhs.perbaruiDokPerpus');
+    });
+    Route::get('/uploadAkademik', function () {
+        return view('dashboard.menuMhs.uploadAkademik');
+    });
+    Route::get('/tambahDokAkademik', function () {
+        return view('dashboard.menuMhs.tambahDokAkademik');
+    });
+    Route::get('/perbaruiDokAkademik', function () {
+        return view('dashboard.menuMhs.perbaruiDokAkademik');
+    });
 });
 
 
